307107dd4f2476d3ff36c114c5247da78edccdf3
timfel20 <timmyfelicia@gmail.com>
2021-11-16 14:16:53 +0100

    consult update

--- updateConsult.php
+++ updateConsult.php
@@ -1,16 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
+
 <body>
-    
 
+    <section class="">
+        <div class="header">
 
+        </div>
+        <div class="first-layer-editar">
+            <div class="second-layer-editar" style='height:80vh'>
+                <div class=third-layer-editar>
+                    <img class="third-layer-img" src="cancel.png" style='' class="d-flex ">
+                    <div class="form-floating mb-3">
+                        <input type="text" class="form-control" id="floatingInput" placeholder="nombre y apellido">
+                        <label for="floatingInput">nombre y apellido</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="email" class="form-control" id="floatingPassword" placeholder="Email con el que se registró la consulta">
+                        <label for="floatingPassword">Email con el que se registró la consulta</label>
+                    </div>
+                    <div class="form-floating">
+                        <input style='height:28vh' type="text" class="form-control " id="floatingPassword" placeholder="Motivo del cambio de cita">
+                        <label for="floatingPassword">Motivo del cambio de cita</label>
+                    </div>
+                    <div class="editar-cita-div">
+                        <button class="editar-cita-button">Enviar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
 
-
+        </div>
+    </section>
 </body>
+
 </html>
