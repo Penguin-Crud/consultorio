31399b9d5fac0fd0f2f74311cc2517cc65abb2ef
El Alklde <alklde@MacBook-Pro.local>
2021-11-25 13:06:37 +0100

    Configuracion local

--- src/Core/SQLConnection.php
+++ src/Core/SQLConnection.php
@@ -7,15 +7,18 @@ use PDOException;
 
 class SQLConnection {
     public $mysql;
-    
+    public $user; 
+    public $pass; 
+
     function __construct(){
+        $config = new Config();
+        $this -> user = $config -> user; 
+        $this -> pass = $config -> pass;
         $host = "localhost";
-        $user = "root";
-        $pass = "";
         $database = "consultorio";
         $charset = "utf-8";
         $options = [PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC];
 
-        $this -> mysql = new PDO("mysql:host={$host};dbname={$database};charset{$charset};", $user, $pass, $options);  
+        $this -> mysql = new PDO("mysql:host={$host};dbname={$database};charset{$charset};", $this -> user, $this -> pass, $options);  
     }
 }
